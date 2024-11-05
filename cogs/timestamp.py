0b2ac549c76eceac599d5a2ff3b36cb747ee2a10
GaiusNale <gaiusnale@gmail.com>
2024-11-05 15:09:05 +0100

    added formatting for the bot's times

--- cogs/timestamp.py
+++ cogs/timestamp.py
@@ -13,12 +13,28 @@ class TimestampCog(commands.Cog):
         self.bot = bot
 
     @app_commands.command(name="timestamp", description="Generate a timestamp in the specified timezone.")
-    async def timestamp(self, interaction: discord.Interaction, tz: str = 'UTC'):
+    async def timestamp(self, interaction: discord.Interaction, tz: str = 'UTC' , time: str = None):
         """Generates a Discord timestamp for the specified timezone."""
         try:
+            # this gets the current date and uses as a base for the user to be able to pick a time (will edit this later to add date functionality as well)
             timezone = pytz.timezone(tz)
-            current_time = datetime.datetime.now(timezone)
-            unix_timestamp = int(current_time.timestamp())
+            current_date = datetime.datetime.now(timezone).date()
+
+            if time:
+                try:
+                    # This collects the time argument in HH:MM
+                    hour, minute = map(int, time.split(":"))
+                    custom_time = datetime.datetime.combine(current_date, datetime.time(hour,minute))
+                    custom_time = timezone.localize(custom_time) # this localizes the time to the current time zone
+                
+                except ValueError:
+                    await interaction.response.send_message("That's an invalid time format you should use the HH:MM format")
+                    return
+            else: 
+                # If no input is given for the specific this defaults back to geting the current time 
+                custom_time = datetime.datetime.now
+
+            unix_timestamp = int(custom_time.timestamp())
             await interaction.response.send_message(f"<t:{unix_timestamp}:F> in {tz} timezone")
             logger.info(f"Timestamp command executed for timezone: {tz}")
         except pytz.UnknownTimeZoneError:
